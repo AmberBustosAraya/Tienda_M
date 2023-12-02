3981a39ec513e0eb23c196f697983453be321d9d
Amber Natasha Bustos Araya <103160154+AmberBustosAraya@users.noreply.github.com>
2023-12-02 07:52:28 -0600

    Add files via upload
    Semana de la 5 a la 12 (Commit así porque tuve muchos problemas con el git hub)

---
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -0,0 +1,42 @@
+package com.tienda.service.impl;
+
+import com.tienda.dao.CategoriaDao;
+import com.tienda.domain.Categoria;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.tienda.service.CategoriaService;
+
+@Service
+public class CategoriaServiceImpl implements CategoriaService {
+
+    @Autowired
+    private CategoriaDao categoriaDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activo) {
+        var categorias = categoriaDao.findAll();
+        return categorias;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
+    }
+
+}
