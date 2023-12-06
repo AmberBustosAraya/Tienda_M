e1e3e4e34a3646d3b39881545d4a8dc3f85920b8
Amber Natasha Bustos Araya <103160154+AmberBustosAraya@users.noreply.github.com>
2023-12-06 17:38:54 -0600

    Commit final
    Portafolio completo

---
+++ com.tienda/src/main/java/com/tienda/TiendaApplication.java
@@ -0,0 +1,14 @@
+package com.tienda;
+
+import org.springframework.boot.SpringApplication;
+import static org.springframework.boot.SpringApplication.run;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaApplication {
+
+	public static void main(String[] args) {
+		run(TiendaApplication.class, args);
+	}
+
+}
